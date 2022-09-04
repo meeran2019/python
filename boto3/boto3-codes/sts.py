8e8b6b4458b2d3a1b2b18d9b24dde22ba6c0975c
meeran2019 <58390359+meeran2019@users.noreply.github.com>
2022-09-04 12:08:51 +0530

    update

---
+++ boto3/boto3-codes/sts.py
@@ -0,0 +1,18 @@
+# import boto3 package. 
+import boto3
+
+# create the aws management console session. 
+aws_man_con = boto3.session.Session()
+
+# create STS client object. 
+sts_client = aws_man_con.client('sts')
+
+# call the caller identity object. 
+response = sts_client.get_caller_identity()
+print(response['Account'])
+
+# get session token credentials.
+response = sts_client.get_session_token()
+print(response['Credentials']['AccessKeyId'])
+print(response['Credentials']['SecretAccessKey'])
+
